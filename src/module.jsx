dac25ec409000c0aeb062773a45555940f62177f
GuzeevZhenya <shyst.evgeny@mail.ru>
2022-07-13 12:10:33 +0300

    done

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import cheese from './img/heart.png';
+
+const Module = () => (
+  <>
+    <h1>Hello,React</h1>
+    <img src={cheese}/>
+  </>
+);
+
+export default Module;
